e3a70427a4fcf95c2c8eeab747a22c701e6b224b
magno309 <luisabrahamsanabriamelecio@gmail.com>
2020-10-28 00:34:17 -0600

    Update ComunicacionServidor.ino

--- ComunicacionServidor/ComunicacionServidor.ino
+++ ComunicacionServidor/ComunicacionServidor.ino
@@ -5,7 +5,7 @@
 const char* ssid     = "ARRIS-A692";
 const char* password = "3AAAA137575F5418";
 
-String nombreServidor = "http://fida-mil.somee.com/ESPToDB.aspx";
+const char* nombreServidor = "http://fida-mil.somee.com/ESPToDB.aspx";
 
 // the following variables are unsigned longs because the time, measured in
 // milliseconds, will quickly become a bigger number than can be stored in an int.
@@ -57,60 +57,34 @@ void leerCantidadComida() {
 
 void enviarCantidadComida() {
   HTTPClient http;
-  String datos_a_enviar = "accion=actCont&idDisp=" + String(idDispensador) + "&pesoAct=" + String(cantidadComida);
+  String datos_a_enviar = "?accion=actCont&idDisp=" + String(idDispensador) + "&pesoAct=" + String(cantidadComida);
   http.begin(nombreServidor);
   http.addHeader("Content-Type", "application/x-www-form-urlencoded");
   int codigo_respuesta = http.POST(datos_a_enviar);
+  Serial.println("Respuesta del servidor: " + String(codigo_respuesta));
   if (codigo_respuesta > 0) {
-    Serial.println("Respuesta del servidor: " + codigo_respuesta);
     if (codigo_respuesta == 200) {
-      String cuerpo_respuesta = http.getString();
-      Serial.println("Respuesta del servidor: " + cuerpo_respuesta);
+      //String cuerpo_respuesta = http.getString();
+      //Serial.println("Respuesta del servidor: " + cuerpo_respuesta);
     } else {
       Serial.println("Error con código " + codigo_respuesta);
     }
-    http.end();
   }
-}
-
-void consultarDespacho() {
-  HTTPClient http;
-  String accion = nombreServidor + "?accion=consultarActivados&idDisp=" + String(idDispensador);
-  String lectura = httpGETRequest(accion);
-  Serial.println(lectura);
-  JSONVar objeto = JSON.parse(lectura);
-
-  if (JSON.typeof(objeto) == "undefined") {
-    Serial.println("Error al parsear entrada!");
-    return;
-  }
-
-  Serial.print("Objeto JSON = ");
-  Serial.println(objeto);
-}
-
-String httpGETRequest(String serverName) {
-  HTTPClient http;
-
-  // Your IP address with path or Domain name with URL path
-  http.begin(serverName);
-
-  // Send HTTP POST request
-  int httpResponseCode = http.GET();
-
-  String payload = "{}";
-
-  if (httpResponseCode > 0) {
-    Serial.print("HTTP Response code: ");
-    Serial.println(httpResponseCode);
-    payload = http.getString();
-  }
-  else {
-    Serial.print("Error code: ");
-    Serial.println(httpResponseCode);
-  }
-  // Free resources
   http.end();
-
-  return payload;
+  /*WiFiClient cliente;
+    if (cliente.connect(nombreServidor, 80)) {
+    Serial.println("Cliente conectado!");
+    String postStr = "?accion=actCont&idDisp=" + String(idDispensador) + "&pesoAct=" + String(cantidadComida);
+    postStr += "\r\n\r\n";
+    cliente.print("POST /ESPToDB.aspx HTTP/1.1\n");
+    cliente.print("Host: http://fida-mil.somee.com\n");
+    cliente.print("accion=actCont&idDisp=" + String(idDispensador) + "&pesoAct=" + String(cantidadComida) + "\n");
+    cliente.print("Content-Type: application/x-www-form-urlencoded\n");
+    cliente.print("Content-Length: ");
+    cliente.print(postStr.length());
+    cliente.print("\n\n");
+    cliente.print(postStr);
+    delay(1000);
+    }
+    cliente.stop();*/
 }
